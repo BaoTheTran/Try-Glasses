bec1b8809c350f4e840609875f6d1772d2e3e61c
BaoTheTran <ttbao2710@gmail.com>
2023-06-25 14:20:53 +0700

    complete project

--- src/tryGlasses/index.js
+++ src/tryGlasses/index.js
@@ -14,12 +14,13 @@ export default class Glasses extends Component {
         };
     }
 
-    renderProduct = (glasses,name,price,desc)=>{
+    renderProduct = (index)=>{
+        const {productList} = this.state;
         this.setState({
-            glasses ,
-            name,
-            price ,
-            desc,
+            glasses: productList[index].url,
+            name : productList[index].name,
+            price : productList[index].price,
+            desc: productList[index].desc,
         })
        
     }
@@ -52,44 +53,41 @@ export default class Glasses extends Component {
         </div> 
 
             <div className="row alert alert-info">
-                <button  className='col-2 btn btn-outline-success' onClick={()=>this.renderProduct(productList[0].url,productList[0].name,productList[0].price,productList[0].desc )}>
+                <button  className='col-2 btn btn-outline-success' onClick={()=>this.renderProduct(0)}>
                     <img src="./glassesImage/g1.jpg" alt="" className='img-fluid'/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g2.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[1].url,productList[1].name,productList[1].price,productList[1].desc )}/>
+                    <img src="./glassesImage/g2.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(1)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g3.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[2].url,productList[2].name,productList[2].price,productList[2].desc )}/>
+                    <img src="./glassesImage/g3.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(2)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g4.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[3].url,productList[3].name,productList[3].price,productList[3].desc)}/>
+                    <img src="./glassesImage/g4.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(3)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success' >
-                    <img src="./glassesImage/g5.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[4].url,productList[4].name,productList[4].price,productList[4].desc)}/>
+                    <img src="./glassesImage/g5.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(4)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g6.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[5].url,productList[5].name,productList[5].price,productList[5].desc)}/>
+                    <img src="./glassesImage/g6.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(5)}/>
                 </button>
             </div>
             <div className="row alert alert-info ">
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g7.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[6].url,productList[6].name,productList[6].price,productList[6].desc)}/>
+                    <img src="./glassesImage/g7.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(6)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g8.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[7].url,productList[7].name,productList[7].price,productList[7].desc)}/>
+                    <img src="./glassesImage/g8.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(7)}/>
                 </button>
                 <button  className='col-2 btn btn-outline-success'>
-                    <img src="./glassesImage/g9.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(productList[8].url,productList[8].name,productList[8].price,productList[8].desc)}/>
+                    <img src="./glassesImage/g9.jpg" alt="" className='img-fluid' onClick={()=>this.renderProduct(8)}/>
                 </button>
             </div>
-
-        
-      </div>)
-
+      </div>);
     }
-    
 
   render() {
+
     return (
         <div>
             {this.renderUI()}
